01cc36f5d2b4107248756f7e437d32289096e71f
Ian.Lintner <lintner.ian@gmail.com>
2016-01-13 14:14:04 -0600

    migrate files

--- import-files.sh
+++ import-files.sh
@@ -8,18 +8,14 @@
 target_file="$HOME/import/files/files-export.tar.gz"
 extracted_dir=$HOME/import/files/files-export
 
+mkdir -p $extracted_dir
+
 echo "extracting file $target_file"
 tar -xvf $target_file -C $extracted_dir
 
 echo "moving files to /mnt/files/$1.$2/$3"
 mv -v $extracted_dir/* /mnt/files/$1.$2/$3
 
-#clean up this server
-echo "removing temp directory files-export"
-rm -rf files-export
-
-echo "removing $target_file"
-rm $target_file
 
 read -p "Do you want to delete archive file? " -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -28,3 +24,4 @@ then
 fi
 echo "removing $file_to_copy on $1"
 rm $file_to_copy
+rm $extracted_dir
