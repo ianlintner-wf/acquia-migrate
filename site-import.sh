0aac2a033c1f5edfab734b7f5c9950c5c8ce27cc
Ian.Lintner <lintner.ian@gmail.com>
2016-01-13 16:57:40 -0600

    site import

--- site-import.sh
+++ site-import.sh
@@ -1,10 +1,17 @@
-#Arg1 source server hostname
-#Arg2 source server site
-#Arg3 source server enviornment
+#!/usr/bin/env bash
+#ARG1 source server hostname
+#ARG2 source server site
+#ARG3 source server enviornment
 
-$source_server=$2.$3@$1
-$export_file="~/export/site.tar.gz"
-$import_file="~/import/site.tar.gz"
+#Arg4 target server site
+#ARG5 target server env
+
+#example
+#./site-import.sh 1234@acquia.devcloud.com pubsite prod pubsite prod
+
+source_server=$2.$3@$1
+export_file="~/export/site.tar.gz"
+import_file="~/import/site.tar.gz"
 ssh $source_server "mkdir -p ~/export"
 ssh $source_server "drush @$2.$3 archive-dump --destination=$export_file"
 scp $source_server:$export_file $import_file
