6f6e10747917b443c58972eb137e20bfa32e5df8
Ian.Lintner <lintner.ian@gmail.com>
2016-01-13 13:38:36 -0600

    export files

--- export-files.sh
+++ export-files.sh
@@ -5,10 +5,10 @@
 #example
 #./export-files.sh workivainternal prod sites/default/files-private
 
-$export_dir=$HOME/export
-$archive=files-export.tar.gz
-$file_path=/mnt/files/$1.$2/$3
-mkdir $export_dir
+export_dir=$HOME/export
+archive=files-export.tar.gz
+file_path=/mnt/files/$1.$2/$3
+mkdir -p $export_dir
 
 tar -zcvf "$export_dir/$archive" -C $file_path .
 echo $export_dir/$archive
