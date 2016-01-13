1d1fb357726537f595e304692260b096a1dcd67d
Ian.Lintner <lintner.ian@gmail.com>
2016-01-13 11:11:47 -0600

    updated export-sql

--- export-sql.sh
+++ export-sql.sh
@@ -13,6 +13,46 @@ else
     $file_to_export="./export/backup.sql.gz"
 fi
 
-backup_dir="/mnt/files/$2.$3/backups/on-demand/"
-file_to_copy="ls $backup_dir/$2-$3-*.sql.gz | head -1"
-cp $file_to_copy $file_to_export
+backup_dir="/mnt/files/$2.$3/backups/on-demand"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+$drush_command="drush @$1.$2 ac-database-instance-backup $3"
+json="$($drush_command --format=json)"
+id="$(echo $json | sed "s/},/\n/g" | sed 's/.*"id":"\([^"]*\)".*/\1/')"
+
+  oldlogs=""
+  while [[ $state != "done" && $state != "error" ]]
+  do
+    # Checking consumes resources, so wait for 3 seconds between checks.
+    sleep 3
+    json="$(drush $1 ac-task-info $id --format=json)"
+    state="$(echo $json | sed "s/},/\n/g" | sed 's/.*"state":"\([^"]*\)".*/\1/')"
+    newlogs="$(echo $json | sed "s/},/\n/g" | sed 's/.*"logs":"\([^"]*\)".*/\1/')"
+    if [[ $newlogs != $oldlogs ]]
+    then
+      logdiff=${newlogs//"$oldlogs"/}
+      logdiff=${logdiff//"\n"/}
+      if [[ $logdiff != "" ]]
+      then
+        echo "    $logdiff"
+      fi
+    fi
+    oldlogs="$newlogs"
+  done
+
+  if [[ $state = "error" ]]
+  then
+    echo -e "    State: ${RED}$state${NC}"
+    exit 1
+  else
+    echo -e "    State: ${GREEN}$state${NC}"
+    echo "copy file"
+    file_to_copy="ls $backup_dir/*$4*.sql.gz | tail -1"
+    cp $file_to_copy $file_to_export
+  fi
+
+
+
