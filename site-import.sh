27a77d8f29da53e48933fc327a914bf3e8c008dc
Ian.Lintner <lintner.ian@gmail.com>
2016-01-13 17:37:40 -0600

    added mkdir

--- site-import.sh
+++ site-import.sh
@@ -9,9 +9,11 @@
 #example
 #./site-import.sh 1234@acquia.devcloud.com pubsite prod pubsite prod
 
+mkdir -p "$HOME/import"
+
 source_server=$2.$3@$1
 export_file="~/export/site.tar.gz"
-import_file="~/import/site.tar.gz"
+import_file="$HOME/import/site.tar.gz"
 ssh $source_server "mkdir -p ~/export"
 ssh $source_server "drush @$2.$3 archive-dump --destination=$export_file"
 scp $source_server:$export_file $import_file
