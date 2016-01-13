e13e5e041fe4a6db96330eae29f0b52aff08c626
Ian.Lintner <lintner.ian@gmail.com>
2016-01-13 12:08:43 -0600

    updated sql migrate

--- migrate-sql.sh
+++ migrate-sql.sh
@@ -10,11 +10,13 @@
 #ARG6 target env
 #ARG7 target db
 
+server=$2.$3@$1
+
 echo "Running sql export on $1"
 ssh $server "bash -s" < ./export-sql.sh "$2" "$3" "$4"
 
 echo "Transfering backup file from $1"
-transfer_sql_backup.sh $1 $2 $3
+./transfer_sql_backup.sh $1 $2 $3
 
 echo "Running drush import on @$5.$6 for db $7"
-import-sql $5 $6 $7
+./import-sql $5 $6 $7
