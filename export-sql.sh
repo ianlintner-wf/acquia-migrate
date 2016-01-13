114761a70a7d8bfcfb42a0b9ce7edab025afec3b
Ian.Lintner <lintner.ian@gmail.com>
2016-01-13 10:57:42 -0600

    updated export-sql

--- export-sql.sh
+++ export-sql.sh
@@ -14,6 +14,5 @@ else
 fi
 
 backup_dir="/mnt/files/$2.$3/backups/on-demand/"
-drush @$1.$2 ac-database-instance-backup $3
 file_to_copy="ls $backup_dir/$2-$3-*.sql.gz | head -1"
 cp $file_to_copy $file_to_export
