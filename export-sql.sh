1e684b051be8519ab40a866eed7498fdcf693bee
Ian.Lintner <lintner.ian@gmail.com>
2016-01-13 10:04:33 -0600

    updated export-sql

--- export-sql.sh
+++ export-sql.sh
@@ -2,7 +2,16 @@
 #ARG1 site
 #ARG2 env
 #ARG3 database
+#ARG4 export file
+
+if [ ! -z $4 ]
+then
+    $file_to_export=$4
+else
+    $file_to_export=$HOME/export/backup.sql.gz
+fi
 
 backup_dir="/mnt/files/$2.$3/backups/on-demand/"
 drush @$1.$2 ac-database-instance-backup $3
 file_to_copy="ls $backup_dir/$2-$3-*.sql.gz | head -1"
+cp $file_to_copy $file_to_export
--- import-sql.sh
+++ import-sql.sh
@@ -1,8 +1,18 @@
 #ARG1 site
 #ARG2 env
 #ARG3 db
+#ARG4 file (optional)
 #Example pubsite dev pubsite
+
 
 #create if does not exist
 echo "Running drush import"
-drush @$1.$2 ah-db-import ~/import/sql/backup.sql.gz --db=$3
+
+if [ ! -z $4 ]
+then
+    $file_to_import=$4
+else
+    $file_to_import=$HOME/import/sql/backup.sql.gz
+fi
+
+drush @$1.$2 ah-db-import $file_to_import --db=$3
