409a888b360bbbca9ab9697fb2c6304502b8eec7
Ian.Lintner <lintner.ian@gmail.com>
2016-01-14 13:40:01 -0600

    uri for multisite

--- import-sql.sh
+++ import-sql.sh
@@ -8,15 +8,22 @@
 #create if does not exist
 echo "Running drush import"
 
-if [ ! -z $4 ]
+if [ ! -z $5 ]
 then
     file_to_import=$4
 else
     file_to_import=$HOME/import/sql/backup.sql.gz
 fi
 
-echo "running import: drush @$1.$2 ah-db-import $file_to_import -l --db=$3 --drop --force"
-drush @$1.$2 ah-db-import $file_to_import -l --db=$3 --drop --force
+if [ ! -z $4 ]
+then
+  echo "running import: drush @$1.$2 ah-db-import $file_to_import --uri=$4 --db=$3 --drop --force"
+  drush @$1.$2 ah-db-import $file_to_import --uri=$4 --db=$3 --drop --force
+
+else
+  echo "running import: drush @$1.$2 ah-db-import $file_to_import  --db=$3 --drop --force"
+  drush @$1.$2 ah-db-import $file_to_import --db=$3 --drop --force
+fi
 
 echo "cleaning up import file"
 rm $file_to_import
--- migrate-sql.sh
+++ migrate-sql.sh
@@ -10,6 +10,8 @@
 #ARG6 target env
 #ARG7 target db
 
+#ARG8 site uri
+
 
 server=$2.$3@$1
 
@@ -20,4 +22,4 @@ echo "Transfering backup file from $1"
 ./transfer-sql-backup.sh $1 $2 $3
 
 echo "Running drush import on @$5.$6 for db $7"
-./import-sql.sh $5 $6 $7
+./import-sql.sh $5 $6 $7 $8
