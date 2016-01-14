dcee9d88742b2333b2baf81747e361bbcff779e9
Ian.Lintner <lintner.ian@gmail.com>
2016-01-14 12:54:24 -0600

    added site url to sql

--- import-sql.sh
+++ import-sql.sh
@@ -1,6 +1,7 @@
 #ARG1 site
 #ARG2 env
 #ARG3 db
+#ARG4 site (optional)
 #ARG4 file (optional)
 #Example pubsite dev pubsite
 
@@ -8,14 +9,21 @@
 #create if does not exist
 echo "Running drush import"
 
-if [ ! -z $4 ]
+
+
+if [ ! -z $5 ]
 then
     file_to_import=$4
 else
     file_to_import=$HOME/import/sql/backup.sql.gz
 fi
 
-drush @$1.$2 ah-db-import $file_to_import --db=$3 --drop --force
+if [ ! -z $4 ]
+then
+    drush @$1.$2 ah-db-import $file_to_import -l $4 --db=$3 --drop --force
+else
+    drush @$1.$2 ah-db-import $file_to_import --db=$3 --drop --force
+fi
 
 echo "cleaning up import file"
 rm $file_to_import
--- migrate-sql.sh
+++ migrate-sql.sh
@@ -10,6 +10,8 @@
 #ARG6 target env
 #ARG7 target db
 
+#ARG8 site url
+
 server=$2.$3@$1
 
 echo "Running sql export on $1"
@@ -19,4 +21,12 @@ echo "Transfering backup file from $1"
 transfer-sql-backup.sh $1 $2 $3
 
 echo "Running drush import on @$5.$6 for db $7"
-./import-sql.sh $5 $6 $7
+if [ ! -z $4 ]
+then
+    ./import-sql.sh $5 $6 $7 $8
+else
+    drush @$1.$2 ah-db-import $file_to_import --db=$3 --drop --force
+fi
+
+
+
