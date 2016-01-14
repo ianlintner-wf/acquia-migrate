eac40baee3f1b28c505f6f8cd9b3a715afc1de3c
Ian.Lintner <lintner.ian@gmail.com>
2016-01-14 13:16:01 -0600

    fixed pathing

--- import-sql.sh
+++ import-sql.sh
@@ -1,7 +1,6 @@
 #ARG1 site
 #ARG2 env
 #ARG3 db
-#ARG4 site (optional)
 #ARG4 file (optional)
 #Example pubsite dev pubsite
 
@@ -9,21 +8,14 @@
 #create if does not exist
 echo "Running drush import"
 
-
-
-if [ ! -z $5 ]
+if [ ! -z $4 ]
 then
     file_to_import=$4
 else
     file_to_import=$HOME/import/sql/backup.sql.gz
 fi
 
-if [ ! -z $4 ]
-then
-    drush @$1.$2 ah-db-import $file_to_import -l $4 --db=$3 --drop --force
-else
-    drush @$1.$2 ah-db-import $file_to_import --db=$3 --drop --force
-fi
+drush @$1.$2 ah-db-import $file_to_import -l --db=$3 --drop --force
 
 echo "cleaning up import file"
 rm $file_to_import
