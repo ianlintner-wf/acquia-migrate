6bb5b91f4439d8ef5272cfff73ed128946bf731d
Ian.Lintner <lintner.ian@gmail.com>
2016-01-15 11:36:04 -0600

    switched from move to copy

--- export-files.sh
+++ export-files.sh
@@ -2,6 +2,7 @@
 #ARG1 site
 #ARG2 env
 #ARG3 file path
+#ARG4 date
 #example
 #./export-files.sh workivainternal prod sites/default/files-private
 
@@ -10,5 +11,14 @@ archive=files-export.tar.gz
 file_path=/mnt/files/$1.$2/$3
 mkdir -p $export_dir
 
-tar -zcvf "$export_dir/$archive" -C $file_path .
+
+if [ ! -z $4 ]
+then
+  echo "tar -zcvf --after-date=$4 \"$export_dir/$archive\" -C $file_path ."
+  tar -zcvf --after-date=$4 "$export_dir/$archive" -C $file_path .
+else
+  echo "tar -zcvf \"$export_dir/$archive\" -C $file_path ."
+  tar -zcvf  "$export_dir/$archive" -C $file_path .
+fi
+
 echo $export_dir/$archive
